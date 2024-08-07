b7b20869c46cfe36f9f65571ec367c9c299585ac
dewa-4261 <dewangwagh4261@gmail.com>
2024-08-07 23:07:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("dewa")
+
+let age = ;
+ 
+if(18 > age > 10){
+console.log("you can not drive")
+}
+
+else if(age <10){
+    console.log("you are underage")
+}
+else if(age > 50){
+    console.log("you are Overage")
+}
+else{
+    console.log("you can drive")
+}
